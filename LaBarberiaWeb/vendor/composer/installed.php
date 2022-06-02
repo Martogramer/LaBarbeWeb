6f787e8d622e75c243fd72042553e0f0c93c93a4
Martin <martin.iribass@gmail.com>
2022-06-02 18:42:20 -0300

    2

---
+++ LaBarberiaWeb/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'user/la-barberia-web',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9dc37105b2ed5fcb98f780fcd128b938f14ed1d1',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'user/la-barberia-web' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9dc37105b2ed5fcb98f780fcd128b938f14ed1d1',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
